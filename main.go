19b27284d27159b5f80f3b73c266e865c3305920
Bima Setya Nugraha <setyabima016@gmail.com>
2024-07-11 16:35:00 +0700

    service register

--- main.go
+++ main.go
@@ -17,10 +17,14 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
-	user := user.User{
-		Name: "test Name",
-	}
+	userService := user.NewService(userRepository)
 
-	userRepository.Save(user)
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "tes dengan service 1"
+	userInput.Email = "mail@mail.com"
+	userInput.Occupation = "developer"
+	userInput.Password = "password1"
+
+	userService.RegisterUser(userInput)
 
 }
